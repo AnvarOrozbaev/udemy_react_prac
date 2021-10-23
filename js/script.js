dacd1be1f464e65844c5a0aa3786bba4d043887a
Anvar <voloklim@gmail.com>
2021-10-23 15:27:58 +0600

    fetch

--- js/script.js
+++ js/script.js
@@ -235,34 +235,56 @@ const r = new MenuCard(
                 `;
                 form.insertAdjacentElement( 'afterend',statusMessage);
 
-                const request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
+                // const request = new XMLHttpRequest();
+                // request.open('POST', 'server.php');
               //  request.setRequestHeader('Content-type', 'multipart/form-data');
                 const formData = new FormData(form);// всегда проверйте формы н а наличие атрибута name
-
+               
+    
                 const object = {};
-                formData.forEach((value, key)=>{
+                formData.forEach(function(value, key){
                     object[key] = value;
-                })
-                const json = JSON.stringify(object)
-                request.send(json);
-
-                request.addEventListener('load', () => {
-                    if(request.status === 200){
-                        console.log(request.response);
-                        showThanksModal( message.success);
-                        form.reset();
-                     
-                            statusMessage.remove();
-                    
-                    } else {
-                        showThanksModal( message.failure);
-                    }
-                })
-
-            })
-
+                });
+    
+                fetch('server.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(object)
+                }).then(data => data.text())
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                }).catch(() => {
+                    showThanksModal(message.failure);
+                }).finally(() => {
+                    form.reset();
+                });
+            });
         }
+               
+                // //request.send(json);
+
+                
+                //         showThanksModal( message.success);)
+                // request.addEventListener('load', () => {
+                //     if(request.status === 200){
+                //         console.log(request.response);
+                //         showThanksModal( message.success);
+                //         form.reset();
+                     
+                //             statusMessage.remove();
+                    
+                //     } else {
+                //         showThanksModal( message.failure);
+                //     }
+                // })
+
+           // })
+
+       // }
 
         function showThanksModal(message){
             const prevModalDialog = document.querySelector('.modal__dialog');
